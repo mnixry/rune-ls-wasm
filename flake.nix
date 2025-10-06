155b05f74e7d737a3ece4225878006602f94eb5f
HexMix <32300164+mnixry@users.noreply.github.com>
2025-10-06 20:03:09 +0800

    feat: support build demo web application by nix

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    pnpm2nix = {
+      url = "github:wrvsrx/pnpm2nix-nzbr/adapt-to-v9";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     rune-rs = {
       url = "github:rune-rs/rune";
       flake = false;
@@ -18,7 +22,13 @@
   };
 
   outputs =
-    { nixpkgs, rust-overlay, ... }@inputs:
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      pnpm2nix,
+      ...
+    }@inputs:
     let
       inherit (nixpkgs) lib;
       forAllSystems = lib.genAttrs nixpkgs.lib.systems.flakeExposed;
@@ -26,7 +36,10 @@
         system:
         import nixpkgs {
           inherit system;
-          overlays = [ (import rust-overlay) ];
+          overlays = [
+            (import rust-overlay)
+            pnpm2nix.overlays.default
+          ];
         }
       );
     in
@@ -35,10 +48,27 @@
         system:
         let
           pkgs = pkgsBySystem.${system};
-          inherit (pkgs) callPackage;
+          inherit (pkgs) callPackage mkPnpmPackage;
+          version = self.shortRev or self.dirtyShortRev or "unknown";
         in
-        {
+        rec {
           rune-ls-wasm = callPackage ./pkgs/rune-ls-wasm.nix { inherit inputs; };
+          demo = mkPnpmPackage {
+            pname = "rune-ls-demo";
+            inherit version;
+            src = ./.;
+            installInPlace = true;
+            prePatch = ''
+              ln -s ${rune-ls-wasm} result
+            '';
+            preConfigure = ''
+              pnpm config set --global manage-package-manager-versions false
+            '';
+            distDir = "packages/demo/dist";
+            packageJSON = pkgs.writers.writeJSON "package.json" (
+              builtins.removeAttrs (lib.importJSON ./package.json) [ "packageManager" ]
+            );
+          };
         }
       );
     };
